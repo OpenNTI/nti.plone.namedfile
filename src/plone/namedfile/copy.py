ebfd55cfc37773c45e6905a42f1e0003a7078c50
Carlos Sanchez <papachoco@gmail.com>
2017-10-19 17:40:25 -0500

    pep8

--- src/plone/namedfile/copy.py
+++ src/plone/namedfile/copy.py
@@ -1,23 +1,34 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Copy hook for proper copying blob data
+"""
+Copy hook for proper copying blob data
 
 This file was borrowed from z3c.blobfile and is licensed under the terms of
 the ZPL.
+
+.. $Id$
 """
-from plone.namedfile.interfaces import INamedBlobFile
-from ZODB.blob import Blob
-from zope.component import adapter
-from zope.copy.interfaces import ICopyHook
-from zope.copy.interfaces import ResumeCopy
-from zope.interface import implementer
 
 import shutil
+
+from ZODB.blob import Blob
+
+from zope.component import adapter
+
+from zope.copy.interfaces import ICopyHook
+from zope.copy.interfaces import ResumeCopy
+
+from zope.interface import implementer
+
+from plone.namedfile.interfaces import INamedBlobFile
 
 
 @implementer(ICopyHook)
 @adapter(INamedBlobFile)
 class BlobFileCopyHook(object):
-    """A copy hook that fixes the blob after copying"""
+    """
+    A copy hook that fixes the blob after copying
+    """
 
     def __init__(self, context):
         self.context = context
