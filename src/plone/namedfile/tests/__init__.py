45adff3b7bf8c6a2be631dcfd53a211abf8d9398
Carlos Sanchez <papachoco@gmail.com>
2017-10-20 15:12:39 -0500

    use nti.testing layer

--- src/plone/namedfile/tests/__init__.py
+++ src/plone/namedfile/tests/__init__.py
@@ -1,8 +1,49 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
 import os
+
+from nti.testing.layers import GCLayerMixin
+from nti.testing.layers import ZopeComponentLayer
+from nti.testing.layers import ConfiguringLayerMixin
+
+import zope.testing.cleanup
+
+
+class SharedConfiguringTestLayer(ZopeComponentLayer,
+                                 GCLayerMixin,
+                                 ConfiguringLayerMixin):
+
+    set_up_packages = ('plone.namedfile',)
+
+    @classmethod
+    def setUp(cls):
+        cls.setUpPackages()
+
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
+
+    @classmethod
+    def testSetUp(cls, unused_test=None):
+        pass
+
+    @classmethod
+    def testTearDown(cls):
+        pass
 
 
 def getFile(filename):
-    """ return contents of the file with the given name """
+    """
+    Return contents of the file with the given name
+    """
     filename = os.path.join(os.path.dirname(__file__), filename)
     return open(filename, 'rb')
--- src/plone/namedfile/tests/test_storable.py
+++ src/plone/namedfile/tests/test_storable.py
@@ -1,43 +1,27 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This file was borrowed from z3c.blobfile and is licensed under the terms of
-# the ZPL.
 
-##############################################################################
-#
-# Copyright (c) 2008 Zope Foundation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-from OFS.Image import Pdata
-from plone.namedfile.file import FileChunk
-from plone.namedfile.file import NamedBlobImage
-from plone.namedfile.testing import PLONE_NAMEDFILE_FUNCTIONAL_TESTING
-from plone.namedfile.tests import getFile
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 import unittest
+
+from plone.namedfile.file import FileChunk
+from plone.namedfile.file import NamedBlobImage
+
+from plone.namedfile.tests import getFile
+from plone.namedfile.tests import SharedConfiguringTestLayer
 
 
 class TestStorable(unittest.TestCase):
 
-    layer = PLONE_NAMEDFILE_FUNCTIONAL_TESTING
+    layer = SharedConfiguringTestLayer
 
-    def setUp(self):
-        pass
-
-    def test_pdata_storable(self):
-        pdata = Pdata(getFile('image.gif').read())
-        fi = NamedBlobImage(pdata, filename=u'image.gif')
-        self.assertEqual(303, fi.getSize())
-
-    def test_str_storable(self):
+    def test_binary_storable(self):
         fi = NamedBlobImage(getFile('image.gif').read(), filename=u'image.gif')
         self.assertEqual(303, fi.getSize())
 
