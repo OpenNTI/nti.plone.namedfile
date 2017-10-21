4f945e7cca585ba398cc750cdca20cefafb5bd45
Carlos Sanchez <papachoco@gmail.com>
2017-10-21 17:01:22 -0500

    re/enable tests

--- src/plone/namedfile/tests/test_blobfile.py
+++ src/plone/namedfile/tests/test_blobfile.py
@@ -1,127 +1,105 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This file was borrowed from z3c.blobfile and is licensed under the terms of
-# the ZPL.
 
-##############################################################################
-#
-# Copyright (c) 2008 Zope Foundation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-from plone.namedfile import storages
-from plone.namedfile.file import NamedBlobFile
-from plone.namedfile.file import NamedBlobImage
-from plone.namedfile.interfaces import INamedBlobFile
-from plone.namedfile.interfaces import INamedBlobImage
-from plone.namedfile.interfaces import IStorage
-from plone.namedfile.tests.test_image import zptlogo
-from zope.component import provideUtility
-from zope.interface.verify import verifyClass
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import equal_to
+from hamcrest import assert_that
+from hamcrest import has_property
+from hamcrest import has_properties
+
+from nti.testing.matchers import validly_provides
+from nti.testing.matchers import verifiably_provides
 
 import struct
-import transaction
 import unittest
 
+import transaction
 
-def registerUtilities():
-    provideUtility(
-        storages.StringStorable(),
-        IStorage,
-        name='__builtin__.str'
-    )
-    provideUtility(
-        storages.UnicodeStorable(),
-        IStorage,
-        name='__builtin__.unicode'
-    )
-    provideUtility(
-        storages.FileChunkStorable(),
-        IStorage,
-        name='plone.namedfile.file.FileChunk'
-    )
-    provideUtility(
-        storages.FileDescriptorStorable(),
-        IStorage,
-        name='__builtin__.file'
-    )
+from plone.namedfile.file import NamedBlobFile
+from plone.namedfile.file import NamedBlobImage
+
+from plone.namedfile.interfaces import INamedBlobFile
+from plone.namedfile.interfaces import INamedBlobImage
+
+from plone.namedfile.tests import getFile
+from plone.namedfile.tests import SharedConfiguringTestLayer
 
 
-@unittest.SkipTest
 class TestImage(unittest.TestCase):
 
-    #layer = PLONE_NAMEDFILE_INTEGRATION_TESTING
-
-    def setUp(self):
-        registerUtilities()
+    layer = SharedConfiguringTestLayer
 
     def _makeImage(self, *args, **kw):
         return NamedBlobImage(*args, **kw)
 
     def testEmpty(self):
-        file = self._makeImage()
-        self.assertEqual(file.contentType, '')
-        self.assertEqual(file.data, '')
+        image = self._makeImage()
+        assert_that(image, 
+                    has_properties('contentType', b'',
+                                   'data', b''))
 
     def testConstructor(self):
-        file = self._makeImage('Data')
-        self.assertEqual(file.contentType, '')
-        self.assertEqual(file.data, 'Data')
+        image = self._makeImage(b'Data')
+        assert_that(image, 
+                    has_properties('contentType', b'',
+                                   'data', b'Data'))
 
     def testMutators(self):
         image = self._makeImage()
 
         image.contentType = 'image/jpeg'
-        self.assertEqual(image.contentType, 'image/jpeg')
+        assert_that(image, 
+                    has_property('contentType', 'image/jpeg'))
 
+        zptlogo = getFile('zptlogo.gif')
         image._setData(zptlogo)
-        self.assertEqual(image.data, zptlogo)
-        self.assertEqual(image.contentType, 'image/gif')
-        self.assertEqual(image.getImageSize(), (16, 16))
+        assert_that(image,
+                    has_properties('data', equal_to(zptlogo),
+                                   'contentType', is_('image/gif')))
+        assert_that(image.getImageSize(), is_((1536, 532)))
 
     def testInterface(self):
-        self.assertTrue(INamedBlobImage.implementedBy(NamedBlobImage))
-        self.assertTrue(verifyClass(INamedBlobImage, NamedBlobImage))
-        self.assertTrue(INamedBlobFile.implementedBy(NamedBlobImage))
-        self.assertTrue(INamedBlobImage.implementedBy(NamedBlobImage))
-        self.assertTrue(verifyClass(INamedBlobFile, NamedBlobImage))
-
+        image = self._makeImage()
+        
+        assert_that(image, validly_provides(INamedBlobFile))
+        assert_that(image, verifiably_provides(INamedBlobFile))
+        
+        assert_that(image, validly_provides(INamedBlobImage))
+        assert_that(image, verifiably_provides(INamedBlobImage))
+ 
     def testDataMutatorWithLargeHeader(self):
         from plone.namedfile.file import IMAGE_INFO_BYTES
         bogus_header_length = struct.pack('>H', IMAGE_INFO_BYTES * 2)
-        data = ('\xff\xd8\xff\xe0' + bogus_header_length +
-                '\x00' * IMAGE_INFO_BYTES * 2 +
-                '\xff\xc0\x00\x11\x08\x02\xa8\x04\x00')
+        data = (b'\xff\xd8\xff\xe0' + bogus_header_length +
+                b'\x00' * IMAGE_INFO_BYTES * 2 +
+                b'\xff\xc0\x00\x11\x08\x02\xa8\x04\x00')
         image = self._makeImage()
         image._setData(data)
         self.assertEqual(image.getImageSize(), (1024, 680))
 
 
-@unittest.SkipTest
 class TestImageFunctional(unittest.TestCase):
 
-    #layer = PLONE_NAMEDFILE_FUNCTIONAL_TESTING
-
-    def setUp(self):
-        registerUtilities()
+    layer = SharedConfiguringTestLayer
 
     def testCopyBlobs(self):
         from zope.copy import copy
-        file = NamedBlobFile()
-        file.data = u'hello, world'
+        source = NamedBlobFile()
+        source.data = u'hello, world'
+
         image = NamedBlobImage()
         image.data = 'some image bytes'
         transaction.commit()
 
-        file_copy = copy(file)
-        self.assertEqual(file_copy.data, file.data)
+        file_copy = copy(source)
+        assert_that(file_copy.data, is_(source.data))
 
         image_copy = copy(image)
-        self.assertEqual(image_copy.data, image.data)
+        assert_that(image_copy.data, is_(image.data))
