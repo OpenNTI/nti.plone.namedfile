1487c561a614fed4e9774c7261b61f405249bc83
Carlos Sanchez <papachoco@gmail.com>
2017-10-21 17:39:39 -0500

    coverage tests

---
+++ src/plone/namedfile/tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_
+from hamcrest import assert_that
+
+import unittest
+
+from plone.namedfile.tests import SharedConfiguringTestLayer
+
+from plone.namedfile.utils.png_utils import process_png
+
+
+class TestUtils(unittest.TestCase):
+
+    layer = SharedConfiguringTestLayer
+
+    def test_png(self):
+        data = b'\x89PNG\r\n\x1a\n\x00\x00\x00\x10\x00\x00\x00\x0f'
+        assert_that(process_png(data),
+                    is_(('image/png', 16, 15)))
